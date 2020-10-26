b116f524544af81da45c448727606c1d6129fb28
Abhisheikh-G <abhisheikhg@gmail.com>
2020-10-26 16:58:07 -0400

    Testing next/dynamic on page performance

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,24 @@
 import React from "react";
 import Hero from "../components/Hero/Hero";
-import Skills from "../components/Skills/Skills";
 import { makeStyles } from "@material-ui/core";
-import Projects from "../components/Projects/Projects";
-import Contact from "../components/Contact/Contact";
+import dynamic from "next/dynamic";
+
+const Projects = dynamic(() => import("../components/Projects/Projects"), {
+  loading: () => <p>...</p>,
+});
+
+const Contact = dynamic(() => import("../components/Contact/Contact"), {
+  loading: () => <p>...</p>,
+});
+
+const Skills = dynamic(() => import("../components/Skills/Skills"), {
+  loading: () => <p>...</p>,
+});
+
+const DynamicComponentWithCustomLoading = dynamic(
+  () => import("../components/hello"),
+  { loading: () => <p>...</p> }
+);
 
 const useStyles = makeStyles((theme) => ({
   image: {
